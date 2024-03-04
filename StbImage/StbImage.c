7eb737decfcdfb347d07977fca632f4037a1b48f
mspielberg <9729801+mspielberg@users.noreply.github.com>
2024-03-04 10:38:03 -0800

    Add support for BC5 compression
    BC5 is a two-channel (RG) format, designed for holding
    tangent-space normal maps.

--- StbImage/StbImage.c
+++ StbImage/StbImage.c
@@ -9,6 +9,18 @@ int GetImageInfo(char const* filename, int* width, int* height, int* numComponen
   return stbi_info(filename, width, height, numComponents);
 }
 
+void GetBytesPerCompressedBlock(int format)
+{
+  switch (format)
+  {
+    case STBIMAGE_FORMAT_BC1:
+      return 8;
+    case STBIMAGE_FORMAT_BC3:
+    case STBIMAGE_FORMAT_BC5:
+      return 16;
+  }
+}
+
 void GetRGBABlock(stbi_uc* img, int imgWidth, int imgHeight, unsigned char* dest, int blockX, int blockY)
 {
   size_t pixelX = (size_t)blockX * 4;
@@ -24,7 +36,37 @@ void GetRGBABlock(stbi_uc* img, int imgWidth, int imgHeight, unsigned char* dest
     memcpy_s(dest + i * 16, 16, img + stride * (pixelY + i) + pixelX * 4, rowSize);
 }
 
-void CompressToBCx(stbi_uc* img, int imgWidth, int imgHeight, int useAlpha, unsigned char* dest)
+/** @brief Extracts the red and green channels of a 4x4 block of pixels.
+ *
+ *  @param img raw RGBA image data
+ *  @param imgWidth width of img in pixels
+ *  @param imgHeight height of img in pixels
+ *  @param dest pointer to destination memory block of at least 32 bytes
+ *  @param blockX the X coordinate of the block to read, where 0 is the left-most edge
+ *  @param blockY the Y coordinate of the block to read, where 0 is the top-most edge
+ */
+void GetRGBlock(stbi_uc* img, int imgWidth, int imgHeight, unsigned char* dest, int blockX, int blockY)
+{
+  size_t pixelX = (size_t)blockX * 4; // left edge of block in pixel coordinates
+  size_t pixelY = (size_t)blockY * 4; // top edge of block in pixel coordinates
+  size_t stride = (size_t)imgWidth * 4; // bytes between rows of img
+  size_t rowSize = pixelX + 4 <= imgWidth ? 16 : (imgWidth - pixelX) * 4;
+  size_t rowCount = pixelY + 4 <= imgHeight ? 4 : imgHeight - pixelY;
+
+  if (rowSize < 16 || rowCount < 4)
+    memset(dest, 0, 32);
+
+  for (size_t y = 0; y < rowCount; y++)
+  {
+    for (size_t x = 0; x < rowSize; x++)
+    {
+      *dest++ = *(img + stride * (pixelY + y) + 4 * (pixelX + x)    ); // red
+      *dest++ = *(img + stride * (pixelY + y) + 4 * (pixelX + x) + 1); // green
+    }
+  }
+}
+
+void CompressToBC1(stbi_uc* img, int imgWidth, int imgHeight, unsigned char* dest)
 {
   int blockWidth = (imgWidth + 3) / 4;
   int blockHeight = (imgHeight + 3) / 4;
@@ -35,14 +77,64 @@ void CompressToBCx(stbi_uc* img, int imgWidth, int imgHeight, int useAlpha, unsi
     for (int blockX = 0; blockX < blockWidth; blockX++)
     {
       GetRGBABlock(img, imgWidth, imgHeight, rgbaBlock, blockX, blockY);
-      int offset = ((blockWidth * blockY) + blockX) * (useAlpha ? 16 : 8);
-      stb_compress_dxt_block(dest + offset, rgbaBlock, useAlpha, STB_DXT_HIGHQUAL);
+      int offset = ((blockWidth * blockY) + blockX) * 8;
+      stb_compress_dxt_block(dest + offset, rgbaBlock, /* alpha */ 0, STB_DXT_HIGHQUAL);
     }
   }
 }
 
+void CompressToBC3(stbi_uc* img, int imgWidth, int imgHeight, unsigned char* dest)
+{
+  int blockWidth = (imgWidth + 3) / 4;
+  int blockHeight = (imgHeight + 3) / 4;
+  unsigned char rgbaBlock[64];
+
+  for (int blockY = 0; blockY < blockHeight; blockY++)
+  {
+    for (int blockX = 0; blockX < blockWidth; blockX++)
+    {
+      GetRGBABlock(img, imgWidth, imgHeight, rgbaBlock, blockX, blockY);
+      int offset = ((blockWidth * blockY) + blockX) * 16;
+      stb_compress_dxt_block(dest + offset, rgbaBlock, /* alpha */ 1, STB_DXT_HIGHQUAL);
+    }
+  }
+}
+
+void CompressToBC5(stbi_uc* img, int imgWidth, int imgHeight, unsigned char* dest)
+{
+  int blockWidth = (imgWidth + 3) / 4;
+  int blockHeight = (imgHeight + 3) / 4;
+  unsigned char rgBlock[32];
+
+  for (int blockY = 0; blockY < blockHeight; blockY++)
+  {
+    for (int blockX = 0; blockX < blockWidth; blockX++)
+    {
+      GetRGBlock(img, imgWidth, imgHeight, rgBlock, blockX, blockY);
+      int offset = ((blockWidth * blockY) + blockX) * 16;
+      stb_compress_bc5_block(dest + offset, rgBlock);
+    }
+  }
+}
+
+void CompressToBCx(stbi_uc* img, int imgWidth, int imgHeight, int format, unsigned char* dest)
+{
+  switch (format)
+  {
+    case STBIMAGE_FORMAT_BC1:
+      CompressToBC1(img, imgWidth, imgHeight, dest);
+      break;
+    case STBIMAGE_FORMAT_BC3:
+      CompressToBC3(img, imgWidth, imgHeight, dest);
+      break;
+    case STBIMAGE_FORMAT_BC5:
+      CompressToBC5(img, imgWidth, imgHeight, dest);
+      break;
+  }
+}
+
 int CompressMipmapFullScale(
-  stbi_uc* img, int imgWidth, int imgHeight, int useAlpha,
+  stbi_uc* img, int imgWidth, int imgHeight, int format,
   unsigned char* scaleBuf, unsigned char* dest, int destSize, int mipmapLevel)
 {
   int mipmapWidth = imgWidth >> mipmapLevel;
@@ -52,7 +144,7 @@ int CompressMipmapFullScale(
 
   int mipmapBlockWidth = (mipmapWidth + 3) / 4;
   int mipmapBlockHeight = (mipmapHeight + 3) / 4;
-  int mipmapCompressedSize = mipmapBlockHeight * mipmapBlockWidth * (useAlpha ? 16 : 8);
+  int mipmapCompressedSize = mipmapBlockHeight * mipmapBlockWidth * GetBytesPerCompressedBlock(format);
   if (mipmapCompressedSize > destSize)
     return 0;
 
@@ -65,13 +157,13 @@ int CompressMipmapFullScale(
       scaleBuf, mipmapWidth, mipmapHeight, 0, 4);
   }
 
-  CompressToBCx(scaleBuf, mipmapWidth, mipmapHeight, useAlpha, dest);
+  CompressToBCx(scaleBuf, mipmapWidth, mipmapHeight, format, dest);
 
   return mipmapCompressedSize;
 }
 
 int CompressMipmapRepeated(
-  stbi_uc* img, int imgWidth, int imgHeight, int useAlpha,
+  stbi_uc* img, int imgWidth, int imgHeight, int format,
   unsigned char* scaleBuf, unsigned char* dest, int destSize, int mipmapLevel)
 {
   int sourceMipmapLevel = mipmapLevel - 1;
@@ -84,7 +176,7 @@ int CompressMipmapRepeated(
 
   int mipmapBlockWidth = (mipmapWidth + 3) / 4;
   int mipmapBlockHeight = (mipmapHeight + 3) / 4;
-  int mipmapCompressedSize = mipmapBlockHeight * mipmapBlockWidth * (useAlpha ? 16 : 8);
+  int mipmapCompressedSize = mipmapBlockHeight * mipmapBlockWidth * GetBytesPerCompressedBlock(format);
   if (mipmapCompressedSize > destSize)
     return 0;
 
@@ -92,6 +184,7 @@ int CompressMipmapRepeated(
   stbi_uc* scaleDest = mipmapLevel % 2 == 1 ? scaleBuf : img;
   if (mipmapLevel == 0)
   {
+    // skip scaling, just use img directly as the compression source
     scaleDest = img;
   }
   else
@@ -101,13 +194,23 @@ int CompressMipmapRepeated(
       scaleDest, mipmapWidth, mipmapHeight, 0, 4);
   }
 
-  CompressToBCx(scaleDest, mipmapWidth, mipmapHeight, useAlpha, dest);
+  CompressToBCx(scaleDest, mipmapWidth, mipmapHeight, format, dest);
 
   return mipmapCompressedSize;
 }
 
-int ReadImageAsBCx(char const* filename, int flipVertically, int useAlpha, unsigned char* dest, int destSize)
+int ReadImageAsBCx(char const* filename, int flipVertically, int format, unsigned char* dest, int destSize)
 {
+  switch (format)
+  {
+    case STBIMAGE_FORMAT_BC1:
+    case STBIMAGE_FORMAT_BC3:
+    case STBIMAGE_FORMAT_BC5:
+      break;
+    default:
+      return 0;
+  }
+
   int imgWidth, imgHeight, channels_in_file;
   stbi_set_flip_vertically_on_load(flipVertically);
   stbi_uc* img = stbi_load(filename, &imgWidth, &imgHeight, &channels_in_file, 4);
@@ -116,12 +219,17 @@ int ReadImageAsBCx(char const* filename, int flipVertically, int useAlpha, unsig
 
   stbi_uc* scaleBuf =
     (stbi_uc*)malloc((size_t)imgWidth * (size_t)imgHeight / 2 /* 50% width */ / 2 /* 50% height */ * 4 /* channels */);
+  if (!scaleBuf)
+  {
+    stbi_image_free(img);
+    return 0;
+  }
 
   int bytesWritten = 0;
   int mipmapLevel = 0;
   do
   {
-    bytesWritten = CompressMipmapRepeated(img, imgWidth, imgHeight, useAlpha, scaleBuf, dest, destSize, mipmapLevel);
+    bytesWritten = CompressMipmapRepeated(img, imgWidth, imgHeight, format, scaleBuf, dest, destSize, mipmapLevel);
     dest += bytesWritten;
     destSize -= bytesWritten;
     mipmapLevel++;
--- StbImage/StbImage.h
+++ StbImage/StbImage.h
@@ -1,5 +1,10 @@
 #pragma once
+
+#define STBIMAGE_FORMAT_BC1 1
+#define STBIMAGE_FORMAT_BC3 3
+#define STBIMAGE_FORMAT_BC5 5
+
 #define DLLEXPORT __declspec(dllexport)
 DLLEXPORT int GetImageInfo(char const* filename, int* width, int* height, int* numComponents);
-DLLEXPORT int ReadImageAsBCx(char const* filename, int flipVertically, int useAlpha, unsigned char* dest, int destSize);
+DLLEXPORT int ReadImageAsBCx(char const* filename, int flipVertically, int format, unsigned char* dest, int destSize);
 DLLEXPORT int ReadImageAsRGBA(char const* filename, int flipVertically, unsigned char* dest, int destSize);
